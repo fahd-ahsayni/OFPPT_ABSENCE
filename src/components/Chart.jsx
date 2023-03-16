30c8596401a87a4b38f4aedac6cb39be6f65cc8a
fahd ahsyani <fahdahsayni@gmail.com>
2023-03-16 11:37:29 +0100

    Add Dashbord

---
+++ src/components/Chart.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useState } from "react";
+import Chart from "react-apexcharts";
+
+export default function Charts() {
+  const [state, setState] = useState({
+    options: {
+      colors: ["#16a34a", "#2563eb"],
+      chart: {
+        id: "basic-bar",
+      },
+      xaxis: {
+        categories: [
+          "DD201",
+          "DD202",
+          "DD203",
+          "DI201",
+          "DI202",
+          "GE201",
+          "GE202",
+          "GE203",
+        ],
+      },
+    },
+    series: [
+      {
+        name: "Absence rates",
+        data: [30, 40, 45, 50, 49, 60, 70, 91],
+      },
+      {
+        name: "Presence rates",
+        data: [70, 60, 45, 90, 79, 60, 70, 61],
+      },
+    ],
+  });
+  return (
+    <>
+      <div className="app">
+        <div className="row">
+          <div className="mixed-chart">
+            <Chart
+              options={state.options}
+              series={state.series}
+              type="bar"
+              width="900"
+            />
+          </div>
+          <div
+            className="chart"
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <Chart
+              options={state.options}
+              series={state.series}
+              type="area"
+              width="400"
+            />
+            <Chart
+              options={state.options}
+              series={state.series}
+              type="line"
+              width="400"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/DashbordHomePage.jsx
+++ src/pages/DashbordHomePage.jsx
@@ -1,4 +1,5 @@
 import { Typography } from "@material-tailwind/react";
+import Chart from "../components/Chart";
 import React from "react";
 
 const DashbordHomePage = () => {
@@ -6,8 +7,9 @@ const DashbordHomePage = () => {
     <div className="bg-white py-6 md:px-6 px-0 overflow-y-auto h-full rounded-xl shadow-lg">
       <div className="lg:px-12 md:py-8 py-4 md:px-8 px-2.5">
         <Typography variant="h2" className="text-blue-600">
-          Page Accueil
+          Les statistiques d'absence
         </Typography>
+        <Chart />
       </div>
     </div>
   );
